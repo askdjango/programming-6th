c9259a4f80d9e40422ea5a652e3bdbf3ddc4ccb7
Chinseok Lee <allieuslee@gmail.com>
2017-01-10 10:03:16 +0900

    added 0270110.ipynb & myimport 예시 추가

---
+++ 20170110/myimport1.py
@@ -0,0 +1,7 @@
+import myimport2
+
+def main():
+    print(myimport2.mymultiply(1, 2))
+
+if __name__ == '__main__':
+    main()
---
+++ 20170110/myimport2.py
@@ -0,0 +1,8 @@
+
+def mymultiply(x, y):
+    return x * y
+
+# print("myimport2 __name__ : " + __name__)
+
+if __name__ == '__main__':
+    print(mymultiply(1, 2))
