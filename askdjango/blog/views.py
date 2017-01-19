bef4678bd18cc156d368ad9c7bc33ee09fa0e59a
Chinseok Lee <allieuslee@gmail.com>
2017-01-19 12:46:53 +0900

    템플릿을 이용해 blog.views.post_list 응답 생성

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -2,20 +2,4 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 def post_list(request):
-    return HttpResponse("""
-<!doctype html>
-<html>
-<head>
-<meta charset="utf-8" />
-<title>장고 블로그</title>
-<style>
-body {
-    background-color: pink;
-}
-</style>
-</head>
-<body>
-    안녕. 장고야. 넌 참 아름답구나.
-</body>
-</html>
-""")
+    return render(request, 'blog/post_list.html')
