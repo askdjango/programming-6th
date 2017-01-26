60ae1fdc1cb6a6305220df9bce4ac4148831a4a4
Chinseok Lee <allieuslee@gmail.com>
2017-01-26 12:51:35 +0900

    blog.models.Post 에 대한 커스텀 admin 등록

--- askdjango/blog/admin.py
+++ askdjango/blog/admin.py
@@ -2,5 +2,16 @@ from django.contrib import admin
 from blog.models import Post
 
 
-admin.site.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content_length', 'lnglat', 'created_at']
+    list_display_links = ['title']
+    list_filter = ['created_at']
+    search_fields = ['title']
+
+    def content_length(self, post):
+        return '{}글자'.format(len(post.content))
+    content_length.short_description = '글자수'
+
+
+admin.site.register(Post, PostAdmin)
 
--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -17,3 +17,6 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
+    def __str__(self):
+        return self.title
+
