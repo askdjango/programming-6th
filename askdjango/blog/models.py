3eac591fc362b6270ebcd0ffbad8967ae9a2b72e
Chinseok Lee <allieuslee@gmail.com>
2017-01-26 10:58:31 +0900

    Post 모델 admin 등록 및 Custom Validator 적용

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,9 +1,18 @@
+import re
 from django.db import models
+from django.forms import ValidationError
+
+
+def lnglat_validator(value):
+    if not re.match(r'^([+-]?\d+\.?\d*),([+-]?\d+\.?\d*)$', value):
+        raise ValidationError('Invalid LngLat Type')
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)  # descriptor syntax
-    content = models.TextField()
+    title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목으로 노출됩니다. 최대 100자까지 지원됩니다.')  # descriptor syntax
+    content = models.TextField(verbose_name='내용')
+    lnglat = models.CharField(max_length=50, blank=True,
+            validators=[lnglat_validator])
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
