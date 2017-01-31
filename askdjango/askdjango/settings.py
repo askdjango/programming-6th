f2038fe7e0aad86dcb5223464913b38a5ad8eafc
Chinseok Lee <allieuslee@gmail.com>
2017-01-31 10:03:09 +0900

    webtoon 앱 추가

--- askdjango/askdjango/settings.py
+++ askdjango/askdjango/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'debug_toolbar',
     # local apps
     'blog',
+    'webtoon',
 ]
 
 MIDDLEWARE = [
---
+++ askdjango/webtoon/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+
+class Webtoon(models.Model):
+    title = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
