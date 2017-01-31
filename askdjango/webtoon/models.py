ee95fb33535c2ab9a164c527685ad2450208910f
Chinseok Lee <allieuslee@gmail.com>
2017-01-31 12:20:42 +0900

    webtoon hit 필드 추가

--- askdjango/webtoon/models.py
+++ askdjango/webtoon/models.py
@@ -5,6 +5,7 @@ from django.db import models
 
 class Webtoon(models.Model):
     title = models.CharField(max_length=100)
+    hit = models.PositiveIntegerField(default=0)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
