647028f12c6d3efd3db548e281a103c737192203
Chinseok Lee <allieuslee@gmail.com>
2017-02-02 10:32:21 +0900

    accounts.models.Profile 모델 추가

--- askdjango/accounts/models.py
+++ askdjango/accounts/models.py
@@ -1,3 +1,8 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User)
+    phone = models.CharField(max_length=20)
+    address = models.CharField(max_length=100)
