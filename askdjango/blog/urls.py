62a0a166708b7140de38c847f93310eeec1da8e7
Chinseok Lee <allieuslee@gmail.com>
2017-02-07 09:42:56 +0900

    blog.urls name 지정

--- askdjango/blog/urls.py
+++ askdjango/blog/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from blog import views
 
 urlpatterns = [
-    url(r'^$', views.post_list),
-    url(r'^(?P<pk>\d+)/$', views.post_detail),
+    url(r'^$', views.post_list, name='post_list'),
+    url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
 ]
 
