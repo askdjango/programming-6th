6a220f05c45408fffa060d6e49e61ffde318c534
Chinseok Lee <allieuslee@gmail.com>
2017-02-07 10:02:38 +0900

    blog.models.Post 모델에 get_absolute_url 멤버함수 구현

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,5 +1,6 @@
 import re
 from django.contrib.auth.models import User
+from django.core.urlresolvers import reverse
 from django.db import models
 from django.forms import ValidationError
 
@@ -25,6 +26,9 @@ class Post(models.Model):
 
     def __str__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.pk])
 
     def lat(self):
         if self.lnglat:
