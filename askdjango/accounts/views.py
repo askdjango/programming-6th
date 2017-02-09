4f67acfb3ceef0654f274672c3f325f1fd9b597b
Chinseok Lee <allieuslee@gmail.com>
2017-02-09 11:58:05 +0900

    회원가입 시에 email, first_name, last_name 추가 입력

--- askdjango/accounts/views.py
+++ askdjango/accounts/views.py
@@ -1,16 +1,16 @@
 from django.conf import settings
-from django.contrib.auth.forms import UserCreationForm
 from django.shortcuts import redirect, render
+from accounts.forms import SignupForm
 
 
 def signup(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST, request.FILES)
+        form = SignupForm(request.POST, request.FILES)
         if form.is_valid():
             user = form.save()
             return redirect(settings.LOGIN_URL)
     else:
-        form = UserCreationForm()
+        form = SignupForm()
     return render(request, 'accounts/signup_form.html', {
         'form': form,
     })
