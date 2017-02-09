0c461d5856ecbd5284bdc8ebca343b0ee4052d49
Chinseok Lee <allieuslee@gmail.com>
2017-02-09 09:20:51 +0900

    blog.models.Post 의 title 필드에 최소길이 validator 적용

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,6 +1,7 @@
 import re
 from django.contrib.auth.models import User
 from django.core.urlresolvers import reverse
+from django.core.validators import MinLengthValidator
 from django.db import models
 from django.forms import ValidationError
 
@@ -13,7 +14,9 @@ def lnglat_validator(value):
 class Post(models.Model):
     user = models.ForeignKey(User, related_name='blog_post_set')
     author = models.CharField(max_length=20)
-    title = models.CharField(max_length=100, verbose_name='제목', help_text='포스팅 제목으로 노출됩니다. 최대 100자까지 지원됩니다.')  # descriptor syntax
+    title = models.CharField(max_length=100,
+            validators=[MinLengthValidator(10)],
+            verbose_name='제목', help_text='포스팅 제목으로 노출됩니다. 최대 100자까지 지원됩니다.')  # descriptor syntax
     content = models.TextField(verbose_name='내용')
     lnglat = models.CharField(max_length=50, blank=True,
             validators=[lnglat_validator])
