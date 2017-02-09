306c407fa89ed05ed9c1101014ec91b4142fbab2
Chinseok Lee <allieuslee@gmail.com>
2017-02-09 13:02:15 +0900

    댓글 구현 리포트 미션

--- askdjango/blog/urls.py
+++ askdjango/blog/urls.py
@@ -4,8 +4,12 @@ from blog import views_cbv
 
 urlpatterns = [
     url(r'^$', views.post_list, name='post_list'),
-    url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),
+    url(r'^(?P<pk>\d+)/$', views.post_detail, name='post_detail'),   # TODO: + comment_list
     url(r'^new/$', views.post_new, name='post_new'),
     url(r'^(?P<pk>\d+)/edit/$', views.post_edit, name='post_edit'),
+
+    # TODO: comment_new
+    # TODO: comment_edit
+    # TODO: comment_delete
 ]
 
