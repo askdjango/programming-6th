171cc3f6ea0483032e52fdfc41535e099a75ee33
Chinseok Lee <allieuslee@gmail.com>
2017-02-09 10:49:45 +0900

    로그인 구현

--- askdjango/accounts/urls.py
+++ askdjango/accounts/urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls import url
+from django.contrib.auth.views import login
 from accounts import views
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
+    url(r'^login/$', login, name='login', kwargs={
+        'template_name': 'accounts/login_form.html',
+    }),
 ]
 
