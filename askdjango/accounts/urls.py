03a81bd365ca61bb6aa6119813accfe7598612a8
Chinseok Lee <allieuslee@gmail.com>
2017-02-09 11:37:21 +0900

    레이아웃 템플릿에 회원가입/로그인/로그아웃 링크 노출

--- askdjango/accounts/urls.py
+++ askdjango/accounts/urls.py
@@ -1,11 +1,15 @@
+from django.conf import settings
 from django.conf.urls import url
-from django.contrib.auth.views import login
+from django.contrib.auth.views import login, logout
 from accounts import views
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
     url(r'^login/$', login, name='login', kwargs={
         'template_name': 'accounts/login_form.html',
+    }),
+    url(r'^logout/$', logout, name='logout', kwargs={
+        'next_page': settings.LOGIN_URL,
     }),
     url(r'^profile/$', views.profile, name='profile'),
 ]
