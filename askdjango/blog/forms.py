e35165f29b046faf4e2706b2a97c95e9e2eebcde
Chinseok Lee <allieuslee@gmail.com>
2017-02-14 12:51:40 +0900

    blog.views.post_new 를 통한 파일 업로드 처리

--- askdjango/blog/forms.py
+++ askdjango/blog/forms.py
@@ -5,7 +5,7 @@ from .models import Post, Comment
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = ['author', 'title', 'content']
+        fields = ['author', 'title', 'content', 'photo']
 
     def clean_title(self):
         title = self.cleaned_data.get('title', '')
