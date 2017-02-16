2ec2eed0d888acf32cc2d6b3e4d69b99af663152
Chinseok Lee <allieuslee@gmail.com>
2017-02-16 11:37:37 +0900

    pythonanywhere.com 배포 settings 준비

--- askdjango/askdjango/settings/prod.py
+++ askdjango/askdjango/settings/prod.py
@@ -2,5 +2,19 @@ from .common import *
 
 DEBUG = False
 
-# DATABASES = {}
+STATIC_ROOT = os.path.join(BASE_DIR, 'statc')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'HOST': 'nomade.mysql.pythonanywhere-services.com',
+        'NAME': 'nomade$default',
+        'USER': 'nomade',
+        'PASSWORD': 'qwer1234',
+        'OPTIONS': {
+            'sql_mode': 'traditional',  # strict mode
+        },
+    },
+}
 
